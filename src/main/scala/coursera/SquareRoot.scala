9371443fd8e519131275cbf8980855dfbc84d144
Gagandeep Kalra <gagandeepkalra1994@gmail.com>
2018-04-08 12:19:43 +0530

    SquareRoot code improved, lexical scope and shadowing

--- src/main/scala/coursera/SquareRoot.scala
+++ src/main/scala/coursera/SquareRoot.scala
@@ -4,15 +4,18 @@ object SquareRoot extends App {
 
   def abs(x: Double) = if (x < 0) -x else x
 
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
+  def sqrt(x: Double) = {
 
-  def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) / x < 0.0001
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
 
-  def improve(guess: Double, x: Double) = (guess + x / guess) / 2
+    def isGoodEnough(guess: Double) = abs(guess * guess - x) / x < 0.0001
 
-  def sqrt(x: Double) = sqrtIter(1.0, x)
+    def improve(guess: Double) = (guess + x / guess) / 2
+
+    sqrtIter(1.0)
+  }
 
   println(sqrt(16))
   println(sqrt(0.000001))
--- src/main/scala/test.sc
+++ src/main/scala/test.sc
@@ -1,13 +1,5 @@
-def abs(x: Double) = if (x < 0) -x else x
+def factorial(a: Int, b: Int): Int = {
+  if (a == 0) b else factorial(a - 1, b * a)
+}
 
-def sqrtIter(guess: Double, x: Double): Double =
-  if (isGoodEnough(guess, x)) guess
-  else sqrtIter(improve(guess, x), x)
-
-def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) / x < 0.0001
-
-def improve(guess: Double, x: Double) = (guess + x / guess) / 2
-
-def sqrt(x: Double) = sqrtIter(1.0, x)
-
-sqrt(16)
+factorial(5, 1)
