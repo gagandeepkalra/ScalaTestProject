72c47084765c1fd5772cf723a7ba720428f61874
Gagandeep Kalra <gagandeepkalra1994@gmail.com>
2019-01-23 20:28:34 +0530

    codeforces: 1108 A

---
+++ src/main/scala/codeforces/contest1108/TwoDistinctPoints.scala
@@ -0,0 +1,19 @@
+package codeforces.contest1108
+
+object TwoDistinctPoints {
+  def main(args: Array[String]): Unit = {
+    val q = io.StdIn.readInt()
+    (1 to q).foreach { _ =>
+      val Array(l1, r1, l2, r2) = io.StdIn.readLine.split(" ").map(_.toInt)
+
+      val result: List[(Int, Int)] = for {
+        a <- List(l1, r1)
+        b <- List(l2, r2)
+        if a != b
+      } yield (a, b)
+
+      println(result.head._1 + " " + result.head._2)
+    }
+  }
+
+}
