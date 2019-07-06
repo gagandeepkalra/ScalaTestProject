3550bec94bc4f8adbd98e414f51c208606639584
Kalra <gagandeepkalra1994@gmail.com>
2019-07-06 11:03:13 +0800

    solved contest 1175 problem C

---
+++ src/main/scala/codeforces/Electrification.scala
@@ -0,0 +1,20 @@
+package codeforces
+
+/*
+https://codeforces.com/contest/1175/problem/C
+ */
+object Electrification {
+  def main(args: Array[String]): Unit = {
+    println((1 to io.StdIn.readInt).map { _ =>
+      val Array(n, k) = io.StdIn.readLine().split(" ").map(_.toInt)
+      val seq = io.StdIn.readLine().split(" ").map(_.toInt)
+
+      (0 until n - k).map {
+        i =>
+          val res = (seq(i + k) + seq(i)) / 2
+          (res, (seq(i + k) - res) max (res - seq(i)))
+      }.minBy(_._2)._1
+
+    }.mkString("\n"))
+  }
+}
