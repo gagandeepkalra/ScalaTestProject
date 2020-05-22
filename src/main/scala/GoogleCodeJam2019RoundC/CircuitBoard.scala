c4528494ff4b57890a3ab2595f266c4d4510e6ed
Gagandeep kalra <gagandeepkalra1994@gmail.com>
2020-05-22 22:02:28 +0800

    updated CircuitBoard and Shifts to use the generic SegmentTree

---
+++ src/main/scala/GoogleCodeJam2019RoundC/CircuitBoard.scala
@@ -0,0 +1,56 @@
+package GoogleCodeJam2019RoundC
+
+import algorithms.SegmentTree
+import algorithms.utils.ArrayUtils.largestRectangleInAHistogram
+
+object CircuitBoard {
+
+  def main(args: Array[String]): Unit = {
+    val testCases = io.StdIn.readInt()
+
+    for (t <- 1 to testCases) {
+
+      val Array(rows, cols, k) = io.StdIn.readLine().split(" ").map(_.toInt)
+
+      val grid = (0 until rows).map(_ => io.StdIn.readLine.split(" ").map(_.toInt))
+
+      val minSegmentTrees = (0 until rows).map(r => SegmentTree[Int](grid(r), _ min _))
+
+      val maxSegmentTrees = (0 until rows).map(r => SegmentTree[Int](grid(r), _ max _))
+
+      def findMaxDifferenceForRow(row: Int)(l: Int, r: Int): Int = {
+        maxSegmentTrees(row).query(l, r) - minSegmentTrees(row).query(l, r)
+      }
+
+      /**
+       *
+       * @param row row index
+       * @param col column index
+       * @param l   binarySearch left index
+       * @param r   binarySearch right index
+       * @return maximum right we can go from (row, col) inclusive
+       */
+      @scala.annotation.tailrec
+      def binarySearch(row: Int, col: Int)(l: Int, r: Int): Int = {
+        if (l > r)
+          l - col
+        else {
+          val m = (l + r) / 2
+          val midResult = findMaxDifferenceForRow(row)(col, m)
+
+          if (midResult <= k)
+            binarySearch(row, col)(m + 1, r)
+          else
+            binarySearch(row, col)(l, m - 1)
+        }
+      }
+
+      val gridWithRightView = (0 until rows).map(r => (0 until cols).map(c => binarySearch(r, c)(c, cols - 1)))
+
+      val result = (0 until cols).map(c => largestRectangleInAHistogram((0 until rows).map(r => gridWithRightView(r)(c)))).max
+
+      println(s"Case #$t: $result")
+    }
+  }
+
+}
--- src/main/scala/google/kickstart/_2019/G/Shifts.scala
+++ src/main/scala/google/kickstart/_2019/G/Shifts.scala
@@ -43,7 +43,7 @@ object Shifts {
       val size = possibleValues.size
       val mappedToRange = mapToRange(possibleValues)
 
-      val tree = new SegmentTree(new Array[Int](size), _ + _)
+      val tree = SegmentTree[Int](0, size, _ + _)
 
       @scala.annotation.tailrec
       def loop(fList: List[(Long, Long)], sList: List[(Long, Long)], result: Long = 0L): Long = {
